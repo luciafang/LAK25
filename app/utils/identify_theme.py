03f0fd914fe0d615b73c239ad5a4b7bfa039aba3
luciafang <yufang@andrew.cmu.edu>
2024-11-01 18:24:16 -0400

    update with data
    data_feature_labels contain the datasets

--- app/utils/identify_theme.py
+++ app/utils/identify_theme.py
@@ -8,12 +8,32 @@ openai.api_key = "sk-proj-AY1xM1B9DtahgLJm6PoxT3BlbkFJSdVtgdPgQ37SNrDj6eJY"
 
 def split_confused_transcripts(transcript_with_confused_df):
     confused_transcripts_df = transcript_with_confused_df[transcript_with_confused_df.state=='Confused']
-    confused_transcript = ' '.join(confused_transcripts_df.transcript.to_list())
+    # confused_transcript = ' '.join(confused_transcripts_df.transcript.to_list())
     # all_transcripts = ' '.join(transcript_with_confused_df.transcript.to_list())
     all_transcripts = ''
     for line in transcript_with_confused_df.transcript.to_list():
         all_transcripts += str(line)
+
+    confused_transcript = ''
+    for line in confused_transcripts_df.transcript.to_list():
+        confused_transcript += str(line)
+
     return confused_transcript, all_transcripts
+
+def split_confused_transcripts_by_line(transcript_with_confused_df):
+    confused_transcripts_df = transcript_with_confused_df[transcript_with_confused_df.state=='Confused']
+    # confused_transcript = ' '.join(confused_transcripts_df.transcript.to_list())
+    # all_transcripts = ' '.join(transcript_with_confused_df.transcript.to_list())
+    all_transcripts = ''
+    for line in transcript_with_confused_df.transcript.to_list():
+        all_transcripts += (str(line)+'\n')
+
+    confused_transcript = ''
+    for line in confused_transcripts_df.transcript.to_list():
+        confused_transcript += (str(line)+'\n')
+
+    return confused_transcript, all_transcripts
+
 
 
 # send to chatgpt
--- app/utils/locate_timings.py
+++ app/utils/locate_timings.py
@@ -18,7 +18,38 @@ def bin_confusion(confusion_csv, bin_size=30, source_duration=2400):
         emotion, counts = np.unique(df.iloc[row, 1:].values, return_counts=True)
         idx_none = emotion == 'none'
         emotion_not_none = emotion[~idx_none]
-        ratio = counts[~idx_none]
+        # ratio = counts[~idx_none]
+        ratio = counts[~idx_none] / np.sum(counts[~idx_none])
+        # for emo, rats in zip(emotion_not_none, ratio):
+        #     emotion_ratios[emo] = rats
+        for emo, rats in zip(emotion_not_none, ratio):
+            if emo == 'Confused':
+                confusion_vals = rats
+        # all_emotion_ratios.append(emotion_ratios)
+        all_emotion_ratios.append(confusion_vals)
+
+
+    avg_1min = []
+    for i in range(0, source_duration, bin_size):
+        avg_1min.append(np.mean(all_emotion_ratios[i:i + bin_size]))
+
+
+    return avg_1min
+
+def bin_confusion_from_df(df, bin_size=30, source_duration=2400):
+
+    df.fillna(value='none', inplace=True)
+    emotions_list = ['Not Confused', 'Confused']
+
+    all_emotion_ratios = []
+    for row in range(df.shape[0]):
+        # emotion_ratios = {key:0 for key in emotions_list}
+        # confusion_vals = []
+        emotion, counts = np.unique(df.iloc[row, 1:].values, return_counts=True)
+        idx_none = emotion == 'none'
+        emotion_not_none = emotion[~idx_none]
+        # ratio = counts[~idx_none]
+        ratio = counts[~idx_none] / np.sum(counts[~idx_none])
         # for emo, rats in zip(emotion_not_none, ratio):
         #     emotion_ratios[emo] = rats
         for emo, rats in zip(emotion_not_none, ratio):
@@ -37,15 +68,61 @@ def bin_confusion(confusion_csv, bin_size=30, source_duration=2400):
 
 # go to transcript, find the locate timestamp
 def confusion2transcript(trancript_csv, avg_1min):
-    a= np.mean(avg_1min)
-    b = np.std(avg_1min)
-    c = a+b
-    confusion_top5_percent_min = np.array(np.where(avg_1min > c)[0])
+    # a= np.mean(avg_1min)
+    # b = np.std(avg_1min)
+    # c = a+b
+    # top_confusion_min = np.array(np.where(avg_1min > c)[0])
+
+    a = np.min(avg_1min)
+    avg_1min_above_min = avg_1min / a
+    c = 2
+    top_confusion_min = np.array(np.where(avg_1min_above_min > c)[0])
 
     df = pd.read_csv(trancript_csv)
     df['state']='Not Confused'
-    for min in confusion_top5_percent_min:
-        df.state[(df.timestamp < f"00:{min}:00")
-                  & (df.timestamp >= f"00:{min-1}:00")] = 'Confused'
+    print(top_confusion_min)
+    for min in top_confusion_min:
+        print(min)
+        prev_min = min-1
+        prev_min_zfill = str(prev_min).zfill(2)
+        print(prev_min_zfill)
+        min_zfill = str(min).zfill(2)
+        print(min_zfill)
+
+        df.state[(df.timestamp < normalize_time(f"00:{min_zfill}:00")) &
+                 (df.timestamp >= normalize_time(f"00:{prev_min_zfill}:00"))] = 'Confused'
     return df
 
+
+# def normalize_time(time_str):
+#     # Split the time string into hours, minutes, and seconds
+#     hours, minutes, seconds = map(int, time_str.split(':'))
+#
+#     # Calculate the total minutes and then the new hours and minutes
+#     total_minutes = minutes
+#     new_hours = hours + total_minutes // 60
+#     new_minutes = total_minutes % 60
+#
+#     # Format the new time string
+#     normalized_time_str = f"{new_hours:02}:{new_minutes:02}:{seconds:02}"
+#
+#     return normalized_time_str
+
+
+def normalize_time(time_str):
+    # Split the time string into hours, minutes, and seconds
+    hours, minutes, seconds = map(float, time_str.split(':'))
+
+    # Calculate the total seconds
+    total_seconds = hours * 3600 + minutes * 60 + seconds
+    print(total_seconds)
+    # Calculate the new hours, minutes, and seconds
+    new_hours = total_seconds // 3600
+    total_seconds %= 3600
+    new_minutes = total_seconds // 60
+    new_seconds = total_seconds % 60
+
+    # Format the new time string
+    normalized_time_str = f"{int(new_hours):02}:{int(new_minutes):02}:{int(new_seconds):02}"
+    print(normalized_time_str)
+    return normalized_time_str
